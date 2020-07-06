8464610a98256892c11e9ddd13d8a1b694deeaff
Matt Riley <mattrileyaus@gmail.com>
2020-07-06 10:09:03 +1000

    refactor

--- src/compose.js
+++ src/compose.js
@@ -1,15 +1,15 @@
 const _ = require('lodash');
 
-const vals = (obj, arg) => {
-    return _.mapValues(obj, val => _.isFunction(val) ? val(arg) : init(val, arg));
+const mapValues = (obj, arg) => {
+    return _.mapValues(obj, val => _.isFunction(val) ? val(arg) : compose(val, arg));
 };
 
-const init = (obj, arg) => {
+const compose = (obj, arg) => {
     const { __modulename, ...entries } = obj;
     if (!__modulename) return obj;
     const product = {};
     const newArg = { [__modulename]: product, ...arg };
-    return Object.assign(product, vals(entries, newArg));
+    return Object.assign(product, mapValues(entries, newArg));
 };
 
-module.exports = init;
+module.exports = compose;
