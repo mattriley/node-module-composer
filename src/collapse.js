b8b4132d5d5d7e40bb176c5d0a753be86a520638
Matt Riley <mattrileyaus@gmail.com>
2020-07-11 11:00:18 +1000

    improve collapse function and remove lodash and flatten dependencies

--- src/collapse.js
+++ src/collapse.js
@@ -1,18 +1,9 @@
-const _ = require('lodash');
-const flatten = require('flat');
-
-module.exports = obj => {
-    const product = Object.entries(flatten(obj)).reduce((acc, [key, val]) => {
-        const path = key.split('.');
-        const lastTwoAreSame = () => new Set(_.takeRight(path, 2)).size === 1;
-        const shouldCollapse = path.length > 1 && lastTwoAreSame();
-        const collapse = () => {
-            const newPath = _.dropRight(path, 1); // remove the duplicate.
-            Object.assign(val, _.get(obj, newPath)); // intentional mutation.
-            return newPath;
-        };
-        const finalPath = shouldCollapse ? collapse() : path;
-        return _.set(acc, finalPath, val);
-    }, {});
-    return Object.assign(obj, product);
+const collapse = (obj, parentObj, parentKey) => {
+    Object.entries(obj).forEach(([key, val]) => {        
+        if (key === parentKey) parentObj[key] = Object.assign(val, parentObj[key]);
+        if (typeof obj === 'object') collapse(val, obj, key);
+    });
+    return obj;
 };
+
+module.exports = collapse;
