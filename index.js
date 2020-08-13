f0f2a0d0e60bce024a30a4f1fc58e4e4f04fc3f4
Matt Riley <mattrileyaus@gmail.com>
2020-08-13 21:39:43 +1000

    return copy of object with own key and option to merge with override object

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-underscore-dangle */
+const merge = require('lodash.merge');
+
 const mapValues = (obj, arg) => {
     const process = val => (val instanceof Function ? val(arg) : compose(val, arg));
     return Object.entries(obj).reduce((acc, [key, val]) => Object.assign(acc, { [key]: process(val) }), {});
@@ -5,22 +8,27 @@ const mapValues = (obj, arg) => {
 
 const compose = (obj, arg) => {
     const { __modulename, ...entries } = obj;
-    if (!__modulename) return obj;
-    const product = {};
+    const product = {}; 
     const newArg = { [__modulename]: product, ...arg };
     return Object.assign(product, mapValues(entries, newArg));
 };
 
 const collapse = (obj, parentObj, parentKey) => {
-    if (!obj) return obj;
-    Object.entries(obj).forEach(([key, val]) => {        
-        if (key === parentKey) {
-            parentObj[key] = Object.assign(val, parentObj[key]);
-            delete val[key];
-        }
-        if (typeof obj === 'object') collapse(val, obj, key);
-    });
+    if (obj && typeof obj === 'object') {
+        Object.entries(obj).forEach(([key, val]) => {        
+            if (key === parentKey) {
+                parentObj[key] = Object.assign(val, parentObj[key]);
+                delete val[key];
+            }
+            collapse(val, obj, key);
+        });
+    }
     return obj;
 };
 
-module.exports = (...args) => collapse(compose(...args));
+module.exports = (obj, arg, override = {}) => {
+    const { __modulename } = obj;
+    if (!__modulename) return obj;
+    const result = collapse(compose({ __modulename, [__modulename]: obj }, arg));
+    return merge(result, result[__modulename], override);
+};
