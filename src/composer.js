2c285f266d15771c8f835116201a038eecd6e72d
Matt Riley <mattrileyaus@gmail.com>
2020-08-23 11:20:10 +1000

    stop accumulating pre-transformed value

--- src/composer.js
+++ src/composer.js
@@ -3,7 +3,6 @@ const collapse = require('./collapse');
 const override = require('./override');
 
 module.exports = (parent, options, cb) => {
-
     options = options || {};
     const overrides = options.overrides || {};
     const acc = {};
@@ -13,10 +12,9 @@ module.exports = (parent, options, cb) => {
         const composed = compose(obj, arg, key);
         const collapsed = collapse({ [key]: composed });
         const transformed = cb ? cb(collapsed[key]) : collapsed;
-        const combined = Object.assign(collapsed, transformed);
-        const overridden = override(combined, overrides);
-        Object.assign(acc, overridden);        
-        return cb ? overridden : overridden[key];
+        const result = override(transformed, overrides);
+        Object.assign(acc, result);        
+        return cb ? result : result[key];
     };
 
     return cb ? (cb(process), acc) : process;
