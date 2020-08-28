abc3828dab1c5465addbf9f5d1519a15dba55ccd
Matt Riley <mattrileyaus@gmail.com>
2020-08-28 13:06:09 +1000

    remove callback interface

--- src/composer.js
+++ src/composer.js
@@ -2,21 +2,16 @@ const compose = require('./compose');
 const collapse = require('./collapse');
 const override = require('./override');
 
-module.exports = (parent, options, cb) => {
+module.exports = (parent, options) => {
     options = options || {};
     const overrides = options.overrides || {};
-    const acc = {};
 
-    const process = (key, arg, cb) => {
+    return (key, arg) => {
         const obj = parent[key];
         const composed = compose(obj, arg, key);
         const collapsed = collapse({ [key]: composed });
-        const transformed = cb ? cb(collapsed[key]) : collapsed;
-        const result = override(transformed, overrides);
-        Object.assign(acc, result);        
-        return cb ? result : result[key];
+        const result = override(collapsed, overrides);
+        return result[key];
     };
-
-    return cb ? (cb(process), acc) : process;
     
 };
