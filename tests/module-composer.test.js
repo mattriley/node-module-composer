ed61f7a09212ec6309b16d3d657d8c0e328c0f06
Matt Riley <mattrileyaus@gmail.com>
2021-12-14 22:23:49 +1100

    refactor tests

--- tests/module-composer.test.js
+++ tests/module-composer.test.js
@@ -2,58 +2,60 @@ const { test } = require('zora');
 
 const composer = require('../');
 
-test('argument is optional', async t => {
-    await new Promise(resolve => {
-        const src = {
-            foo: {
-                fun: ({ foo }) => () => {
-                    t.ok(typeof foo.fun === 'function');
-                    resolve();
-                }
+test('argument is optional', t => {
+    let fooIsFunction = false;
+
+    const src = {
+        foo: {
+            fun: ({ foo }) => () => {
+                fooIsFunction = typeof foo.fun === 'function';
             }
-        };
-    
-        const foo = composer(src)('foo', undefined);
-        foo.fun();
-    });
+        }
+    };
+
+    const foo = composer(src)('foo', undefined);
+    foo.fun();
+    t.ok(fooIsFunction);
 });
 
-test('peer function is invoked with arg', async () => {
-    await new Promise(resolve => {
-        const src = {
-            foo: {
-                fun1: ({ foo }) => () => {
-                    foo.fun2();
-                },
+test('peer function is invoked with arg', t => {
+    let fun2Called = false;
+
+    const src = {
+        foo: {
+            fun1: ({ foo }) => () => {
+                foo.fun2();
+            },
+            fun2: () => () => {
+                fun2Called = true;
+            }
+        }
+    };
+
+    const foo = composer(src)('foo');
+    foo.fun1();
+    t.ok(fun2Called);
+});
+
+test('nested function is invoked', t => {
+    let fun2Called = false;
+
+    const src = {
+        foo: {
+            bar: {
                 fun2: () => () => {
-                    resolve();
+                    fun2Called = true
                 }
+            },
+            fun1: ({ foo }) => () => {
+                foo.bar.fun2();
             }
-        };
-    
-        const foo = composer(src)('foo');
-        foo.fun1();
-    });
-});
+        }
+    };
 
-test('nested function is invoked', async () => {
-    await new Promise(resolve => {
-        const src = {
-            foo: {
-                bar: {
-                    fun2: () => () => {
-                        resolve();
-                    }
-                },
-                fun1: ({ foo }) => () => {
-                    foo.bar.fun2();
-                }
-            }
-        };
-
-        const foo = composer(src)('foo');
-        foo.fun1();
-    });
+    const foo = composer(src)('foo');
+    foo.fun1();
+    t.ok(fun2Called);
 });
 
 test('function name matching parent key is collapsed', async () => {
@@ -68,7 +70,7 @@ test('function name matching parent key is collapsed', async () => {
                 }
             }
         };
-    
+
         const foo = composer(src)('foo');
         foo();
     });
