c42931dabf01bf4222d7ed8527dbe1e2ae8fbd9f
Matt Riley <mattrileyaus@gmail.com>
2022-04-19 10:30:21 +1000

    refactor

--- src/module-composer.js
+++ src/module-composer.js
@@ -3,23 +3,22 @@ const { isObject, isFunction, mapValues, override } = require('./util');
 module.exports = (parent, options = {}) => {
     const modules = { ...parent }, dependencies = mapValues(modules, () => []);
     modules.composition = { modules, dependencies };
-    return (key, arg = {}, customise) => {
-        arg = { ...options.defaults, ...arg };
-        delete arg[key];
+    return (key, args = {}, customise) => {
+        args = { ...options.defaults, ...args };
+        delete args[key];
         const obj = parent[key];
-        const composed = composeRecursive(obj, arg, key);
-        const initialised = customise ? customise(composed) : composed;
-        const module = override({ [key]: initialised }, options.overrides)[key];
-        modules[key] = module;
-        dependencies[key] = Object.keys(arg);
+        const composed = composeRecursive(obj, args, key);
+        const customised = customise ? customise(composed) : composed;
+        modules[key] = override({ [key]: customised }, options.overrides)[key];
+        dependencies[key] = Object.keys(args);
         return { ...modules };
     };
 };
 
-const composeRecursive = (obj, arg, parentKey) => {
+const composeRecursive = (obj, args, parentKey) => {
     if (!isObject(obj)) return obj;
     const product = {};
-    const newArg = { [parentKey]: product, ...arg };
+    const newArg = { [parentKey]: product, ...args };
     const newObj = mapValues(obj, (val, key) => (isFunction(val) ? val(newArg) : composeRecursive(val, newArg, key)));
     return Object.assign(product, newObj);
 };
