3fc7d0dda9f5339fb927001739f6d241b2aef4fd
Matt Riley <mattrileyaus@gmail.com>
2022-04-19 13:07:00 +1000

    extend tests

--- tests/module-composer.test.js
+++ tests/module-composer.test.js
@@ -2,55 +2,72 @@ const { test } = require('zora');
 
 const composer = require('../');
 
-test('dependencies are optional', t => {
-    const inputModules = { foo: {} };
-    const compose = composer(inputModules);
+test('target module unchanged if not composed', t => {
+    const target = { foo: {}, bar: {} };
+    const compose = composer(target);
+    const { composition, ...modules } = compose('foo');
+    t.equal(modules, { foo: {}, bar: {} });
+    t.equal(composition.dependencies, { foo: [], bar: [] });
+});
+
+test('args are optional', t => {
+    const target = { foo: {} };
+    const compose = composer(target); // NOTE: No args.
     const { composition, ...modules } = compose('foo');
     t.equal(modules, { foo: {} });
     t.equal(composition.dependencies, { foo: [] });
 });
 
-test('dependencies are applied', t => {
-    const inputModules = { foo: {} };
-    const compose = composer(inputModules);
+test('args are applied', t => {
+    const target = { foo: {} };
+    const compose = composer(target);
     const { composition, ...modules } = compose('foo', { bar: {} });
     t.equal(modules, { foo: {} });
     t.equal(composition.dependencies, { foo: ['bar'] });
 });
 
 test('defaults are applied', t => {
-    const inputModules = { foo: {} };
+    const target = { foo: {} };
     const defaults = { bar: {} };
-    const compose = composer(inputModules, { defaults });
+    const compose = composer(target, { defaults });
     const { composition, ...modules } = compose('foo');
     t.equal(modules, { foo: {} });
     t.equal(composition.dependencies, { foo: ['bar'] });
 });
 
-test('initialiser is invoked', t => {
-    const inputModules = {
+test('overrides are applied', t => {
+    const target = { foo: {} };
+    const overrides = { foo: { num: 1 } };
+    const compose = composer(target, { overrides });
+    const { composition, ...modules } = compose('foo');
+    t.equal(modules, { foo: { num: 1 } });
+    t.equal(composition.dependencies, { foo: [] });
+});
+
+test('customiser is invoked', t => {
+    const target = {
         foo: {
             setup: () => () => ({ bar: {} })
         }
     };
-    const compose = composer(inputModules);
-    const { composition, ...modules } = compose('foo', undefined, foo => foo.setup());
+    const compose = composer(target);
+    const { composition, ...modules } = compose('foo', {}, foo => foo.setup());
     t.equal(modules, { foo: { bar: {} } });
     t.equal(composition.dependencies, { foo: [] });
 });
 
-test('avoids following non-objects', t => {
-    const inputModules = { foo: { bar: 1 } };
-    const compose = composer(inputModules);
+test('non-objects are returned as-is', t => {
+    const target = { foo: { bar: 1 } };
+    const compose = composer(target);
     const { composition, ...modules } = compose('foo');
     t.equal(modules, { foo: { bar: 1 } });
     t.equal(composition.dependencies, { foo: [] });
 });
 
-test('peer function is invoked with arg', t => {
+test('peer function is invoked with args', t => {
     let fun2Called = false;
 
-    const inputModules = {
+    const target = {
         foo: {
             fun1: ({ foo }) => () => {
                 foo.fun2();
@@ -61,27 +78,7 @@ test('peer function is invoked with arg', t => {
         }
     };
 
-    const compose = composer(inputModules);
-    const { foo } = compose('foo')
-    foo.fun1();
-    t.ok(fun2Called);
-});
-
-test('peer function is invoked with arg', t => {
-    let fun2Called = false;
-
-    const inputModules = {
-        foo: {
-            fun1: ({ foo }) => () => {
-                foo.fun2();
-            },
-            fun2: () => () => {
-                fun2Called = true;
-            }
-        }
-    };
-
-    const compose = composer(inputModules);
+    const compose = composer(target);
     const { foo } = compose('foo')
     foo.fun1();
     t.ok(fun2Called);
