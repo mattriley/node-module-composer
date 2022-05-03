1ec6d5ee4756ceaf63414f93f0757ed9e67f5406
Matt Riley <mattrileyaus@gmail.com>
2022-05-04 08:39:56 +1000

    Unspecified changes

--- src/mermaid-graph.js
+++ src/mermaid-graph.js
@@ -1,6 +1,9 @@
 module.exports = dependencies => {
 
-    const lines = Object.entries(dependencies).map(([key, dep]) => `    ${key}-->${dep};`);
+    const lines = Object.entries(dependencies)
+        .flatMap(([key, deps]) => deps.map(dep => [key, dep]))
+        .map(([key, dep]) => `    ${key}-->${dep};`);
+
     return ['graph TD;', ...lines].join('\n');
 
 };
