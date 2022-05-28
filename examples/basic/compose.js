f201cf8a7ee8fe8757d7f7898c5f2effaa1c6853
Matt Riley <mattrileyaus@gmail.com>
2022-05-28 21:52:36 +1000

    Unspecified changes

--- examples/basic/compose.js
+++ examples/basic/compose.js
@@ -2,9 +2,9 @@ import composer from 'module-composer';
 import modules from './modules';
 
 export default () => {
-    const { compose, composed } = composer(modules);
+    const { compose, composition } = composer(modules);
     const { stores } = compose('stores');
     const { services } = compose('services', { stores });
     const { components } = compose('components', { services });
-    return composed();
+    return composition.modules;
 };
