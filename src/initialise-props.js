69884c44e593b6ea263bafc215e7189c10c8ac3b
Matt Riley <mattrileyaus@gmail.com>
2022-06-28 23:33:28 +1000

    Unspecified changes

---
+++ src/initialise-props.js
@@ -0,0 +1,23 @@
+const util = require('./util');
+const eject = require('./eject');
+const mermaid = require('./mermaid');
+const defaultOptions = require('./default-options');
+
+module.exports = ({ target, userOptions }) => {
+
+    const options = util.merge({}, defaultOptions, userOptions);
+    const config = util.mergeValues({}, options, options.configKeys);
+
+    const props = {
+        defaultOptions, userOptions, options, config, target,
+        modules: { ...target },
+        dependencies: util.mapValues(target, () => []),
+        composedDependencies: {},
+        stats: { totalDuration: 0, modules: {} },
+        mermaid: opts => mermaid(props.dependencies, opts),
+        eject: () => eject(target, props.composedDependencies)
+    };
+
+    return { ...props, props };
+
+};
--- src/module-composer.js
+++ src/module-composer.js
@@ -1,24 +1,11 @@
 const util = require('./util');
-const eject = require('./eject');
-const mermaid = require('./mermaid');
 const timedCompose = require('./timed-compose');
-const defaultOptions = require('./default-options');
+const initialiseProps = require('./initialise-props');
 
 module.exports = (target, userOptions = {}) => {
 
     let ended = false;
-    const options = util.merge({}, defaultOptions, userOptions);
-    const config = util.mergeValues({}, options, options.configKeys);
-
-    const props = {
-        defaultOptions, userOptions, options, config, target,
-        modules: { ...target },
-        dependencies: util.mapValues(target, () => []),
-        composedDependencies: {},
-        stats: { totalDuration: 0, modules: {} },
-        mermaid: opts => mermaid(props.dependencies, opts),
-        eject: () => eject(target, props.composedDependencies)
-    };
+    const { props, options, config } = initialiseProps({ target, userOptions });
 
     const baseCompose = (key, deps = {}) => {
         if (ended) throw new Error('Composition has ended');
