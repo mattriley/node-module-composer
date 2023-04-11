3b8d5e8d02aa53d2c028aa02839a32d3ae5b2c40
Matt Riley <m@ttriley.dev>
2023-04-12 08:49:21 +1000

    Unspecified changes

--- src/core/Options.js
+++ src/core/Options.js
@@ -6,6 +6,6 @@ module.exports = () => {
         privatePrefix: '_',
         configOptionKeys: ['defaultConfig', 'config', 'configs'],
         overrides: {},
-        extensions: false
+        extensions: true
     };
 };
--- src/core/extensions.js
+++ src/core/extensions.js
@@ -3,10 +3,17 @@ const util = require('./util');
 const state = { extensions: {} };
 const register = (name, extension) => Object.assign(state.extensions, { [name]: extension });
 
-const setup = (session, compose, globalThis) => {
+const listEnabled = session => {
     const loaded = Object.keys(state.extensions);
-    const enabled = session.options.extensions === false ? [] : !session.options.extensions ? loaded : loaded.filter(ext => session.options.extensions.includes(ext));
-    return enabled.reduce((acc, name) => {
+    if (session.options.extensions === false) return [];
+    if (session.options.extensions === true) return loaded;
+    if (!session.options.extensions) return loaded;
+    if (Array.isArray(session.options.extensions)) return loaded.filter(ext => session.options.extensions.includes(ext));
+    throw new Error('Failed to interpret extensions');
+};
+
+const setup = (session, compose, globalThis) => {
+    return listEnabled(session).reduce((acc, name) => {
         const ext = state.extensions[name];
         const getState = () => session.state.extensions[name];
         const setState = state => util.set(session.state.extensions, name, { ...getState(), ...state });
