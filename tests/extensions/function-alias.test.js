2c3cffdae4169371899d7877e6933fa6032ff710
Matt Riley <m@ttriley.dev>
2023-08-27 15:11:30 +1000

    Unspecified changes

--- tests/extensions/function-alias.test.js
+++ tests/extensions/function-alias.test.js
@@ -2,7 +2,7 @@ module.exports = ({ test, assert }) => composer => {
 
     require('module-composer/extensions/function-alias');
 
-    test('function aliases', () => {
+    test('function alias', () => {
         const functionAlias = {
             Value: 'Val'
         };
@@ -10,6 +10,17 @@ module.exports = ({ test, assert }) => composer => {
         const target = { mod: { getValues: {} } };
         const { compose } = composer(target);
         const { mod } = compose.opts({ functionAlias })('mod');
+        assert.equal(mod.getVals, mod.getValues);
+    });
+
+    test('function alias composer level options', () => {
+        const functionAlias = {
+            Value: 'Val'
+        };
+
+        const target = { mod: { getValues: {} } };
+        const { compose } = composer(target, { functionAlias });
+        const { mod } = compose('mod');
         assert.equal(mod.getVals, mod.getValues);
     });
 
