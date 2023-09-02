2aa1432b3a28574c9a05aa7bc9f5168df75f8c26
Adam Woods <squirmy.dev@gmail.com>
2023-09-02 11:21:01 +1000

    feat: add experimental typescript types

---
+++ main.d.ts
@@ -0,0 +1,46 @@
+import type { UnionToIntersection } from 'type-fest'
+
+interface CoreOptions {
+    depth: number
+    overrides: object
+    customiser: string
+    configAlias: string
+    freezeConfig: boolean
+    extensions: boolean
+}
+
+interface ExtensionOptions {
+    globalThis: object
+    publicPrefix: string
+    privatePrefix: string
+    functionAlias: object
+    moduleAlias: string[]
+}
+
+type Options = CoreOptions & ExtensionOptions
+
+type Composed = (...args: any) => any
+type Composable = (deps: Modules) => (...args: any) => any
+type Module = Record<PropertyKey, Composable | Composed>
+type Modules = Record<PropertyKey, Module>
+
+type ComposedModule<T extends Module> = {
+    [K in keyof T]: ReturnType<T[K]>
+}
+
+type ModuleParameters<T extends Module, Key = keyof T> =
+    Key extends PropertyKey ? Parameters<T[Key]>[0] : never
+
+type ModuleDependencies<T extends Module> = UnionToIntersection<NonNullable<ModuleParameters<T>>>
+
+type Compose<T extends Modules> = <Path extends keyof T>(path: Path, deps: ModuleDependencies<T[Path]>, opts?: Partial<Options>) => Record<Path, ComposedModule<T[Path]>>
+
+interface Asis<T extends Modules> {
+    asis<Path extends keyof T>(path: Path, opts?: Partial<Options>): Record<Path, T[Path]>
+}
+
+interface Composer<T extends Modules> {
+    compose: Compose<T> & Asis<T>
+}
+
+export default function composer<T extends Modules>(config: T): Composer<T>
