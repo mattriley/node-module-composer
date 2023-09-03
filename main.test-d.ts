b625aff74fa62e6dfd07159f6eb5afafadec7f88
Adam Woods <squirmy.dev@gmail.com>
2023-09-03 15:22:16 +1000

    chore: formatting

--- main.test-d.ts
+++ main.test-d.ts
@@ -5,11 +5,11 @@ import composer from './main';
 type Toy = string
 type Food = string
 
-type ServeFood = (food: Food, bowl: Food[]) => void 
-type FillBowl = (choice: Food, bowl: Food[]) => void 
-type RandomToy = () => Toy 
-type Play = () => void 
-type Eat = () => void 
+type ServeFood = (food: Food, bowl: Food[]) => void
+type FillBowl = (choice: Food, bowl: Food[]) => void
+type RandomToy = () => Toy
+type Play = () => void
+type Eat = () => void
 
 type HumanDeps = {
     food: { serve: ServeFood }
@@ -17,45 +17,45 @@ type HumanDeps = {
 
 type PlayDeps = {
     toys: { randomToy: () => Toy }
-    util: { shuffle: (str: string) => string}
+    util: { shuffle: (str: string) => string }
 }
 
 type EatDeps = {
     human: { fillBowl: FillBowl }
 }
 
-const modules = { 
-    util : {
+const modules = {
+    util: {
         shuffle: (str: string) => [...str].sort(() => Math.random() - 0.5).join('')
     },
 
-    food: { 
+    food: {
         serve: (): ServeFood => (food, bowl) => { bowl.push(food); }
     },
-    
-    human: { 
+
+    human: {
         fillBowl: ({ food }: HumanDeps): FillBowl => (choice, bowl) => food.serve(choice, bowl)
     },
-    
-    toys: { 
+
+    toys: {
         randomToy: (): RandomToy => () => 'yarn'
     },
 
-    cat: { 
+    cat: {
         play: ({ toys, util }: PlayDeps): Play => () => {
             const toy = toys.randomToy();
             util.shuffle(toy);
-        } ,
+        },
 
         eat: ({ human }: EatDeps): Eat => () => {
             const bowl: Food[] = [];
             human.fillBowl('tuna', bowl);
-            
+
             while (bowl.length > 0) {
                 // have a nibble
                 bowl.pop();
             }
-        } 
+        }
     }
 };
 
@@ -78,5 +78,5 @@ const { toys } = compose('toys', { food });
 expectType<{ randomToy: RandomToy }>(toys);
 
 // compose with multiple dependencies across functions
-const { cat } = compose('cat',  { toys, util, human });
+const { cat } = compose('cat', { toys, util, human });
 expectType<{ play: Play, eat: Eat }>(cat);
