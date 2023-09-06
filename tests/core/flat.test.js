710628521c9864d60909f35324daf7c08fc9d67c
Matt Riley <m@ttriley.dev>
2023-09-06 18:13:58 +1000

    Unspecified changes

--- tests/core/flat.test.js
+++ tests/core/flat.test.js
@@ -2,38 +2,38 @@ module.exports = ({ test, assert }) => composer => {
 
     test('substructure is flattened', () => {
         const target = {
-            foobar: {
+            mod: {
                 sub1: { fun1: () => () => 1 },
                 sub2: { fun2: () => () => 2 }
             }
         };
         const { compose } = composer(target);
-        const { foobar } = compose.flat('foobar');
-        assert.deepEqual(foobar.fun1(), 1);
-        assert.deepEqual(foobar.fun2(), 2);
+        const { mod } = compose.flat('mod');
+        assert.deepEqual(mod.fun1(), 1);
+        assert.deepEqual(mod.fun2(), 2);
     });
 
     test('deps are flat', () => {
         const target = {
-            foobar: {
+            mod: {
                 sub1: {
                     fun1: () => () => 1,
-                    fun2: ({ self, foobar }) => () => {
-                        assert.equal(self, foobar)
-                        return foobar.fun1();
+                    fun2: ({ self, mod }) => () => {
+                        assert.equal(self, mod)
+                        return mod.fun1();
                     },
                     sub2: {
-                        fun3: ({ self, foobar }) => () => {
-                            assert.equal(self, foobar)
-                            return foobar.fun2();
+                        fun3: ({ self, mod }) => () => {
+                            assert.equal(self, mod)
+                            return mod.fun2();
                         }
                     }
                 },
             }
         };
         const { compose } = composer(target);
-        const { foobar } = compose.flat('foobar');
-        assert.deepEqual(foobar.fun3(), 1);
+        const { mod } = compose.flat('mod');
+        assert.deepEqual(mod.fun3(), 1);
     });
 
 };
