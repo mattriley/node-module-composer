d0267bb9209f47394845048d13fa76460a555d3d
Matt Riley <m@ttriley.dev>
2023-09-07 17:16:48 +1000

    Unspecified changes

--- src/core/flatten.js
+++ src/core/flatten.js
@@ -2,21 +2,19 @@ module.exports = (target, opts = {}) => {
 
     const delimiter = opts.delimiter || '.'
     const maxDepth = opts.maxDepth
-    const transformKey = opts.transformKey || (key => key)
     const output = {}
 
     function step(object, prev, currentDepth) {
         currentDepth = currentDepth || 1
         Object.keys(object).forEach(function (key) {
             const value = object[key]
-            const isarray = Array.isArray(value)
             const type = Object.prototype.toString.call(value)
             const isobject = type === '[object Object]'
             const newKey = prev
-                ? prev + delimiter + transformKey(key)
-                : transformKey(key)
+                ? prev + delimiter + key
+                : key
 
-            if (!isarray && isobject && Object.keys(value).length &&
+            if (isobject && Object.keys(value).length &&
                 (!opts.maxDepth || currentDepth < maxDepth)) {
                 return step(value, newKey, currentDepth + 1)
             }
